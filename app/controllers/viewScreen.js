94dc2f90293a105bf2e0a2955aa62cf3b96fd156
trs-mark <trs.markt@gmail.com>
2014-03-14 11:25:45 +0800

    simple menu app

---
+++ app/controllers/viewScreen.js
@@ -0,0 +1,38 @@
+function back(e) {
+	$.viewScreen.close();
+}
+
+// put entries on list
+var data = [];
+
+for(var a=0; a<Alloy.Globals.notes.length; a++){
+	var row = Ti.UI.createTableViewRow({
+		height: '35dp',
+		width: '100%',
+		backgroundColor: '#FFFFFF',
+		title: Alloy.Globals.notes[a].entry,
+		c_id: a
+	});
+	data.push(row);
+}
+
+$.list.setData(data);
+
+$.list.addEventListener('click', function(e) {
+	if(e.row.title){
+		var a = e.row.c_id;
+		var note = Ti.UI.createAlertDialog();
+		note.message = Alloy.Globals.notes[a].entry;
+		note.title = Alloy.Globals.notes[a].date.month + ' ' + Alloy.Globals.notes[a].date.day + ', ' + Alloy.Globals.notes[a].date.year + ' - ' + 
+					Alloy.Globals.notes[a].time.hour + ':' + Alloy.Globals.notes[a].time.minute;
+		note.buttonNames = ['Ok','Edit','Delete'];
+		note.addEventListener('click',function(e){
+			if(e.index == 1){
+				//put code for edit here
+			}else if(e.index == 2){
+				//put code for delete here
+			}
+		});
+		note.show();
+	}
+});
