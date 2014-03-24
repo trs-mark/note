a4ce26ef881056c17d24994733f9750fe1d947e6
trs-mark <trs.markt@gmail.com>
2014-03-24 14:17:13 +0800

    revert back test_complete codes

---
+++ app/controllers/addScreen.js
@@ -0,0 +1,41 @@
+function add(e){
+	var note = $.note.value;
+	if(note.trim().length > 0){
+		var newDate = new Date();
+		var hour = newDate.getHours();
+		var minute = newDate.getMinutes();
+		var day = newDate.getDate();
+		var month = newDate.getMonth();
+		var year = newDate.getFullYear();
+		var newNote = {
+			'date':{
+				'month':getMonthStr(month),
+				'day':day,
+				'year':year
+				},
+			'time':{
+				'hour':hour,
+				'minute':minute
+				},
+			'entry':note.trim()
+		};
+		Alloy.Globals.notes.push(newNote);
+		$.note.value = "";
+		alert('Note added.');
+	}else{
+		alert('Please type a note.');
+	}
+}
+
+function back(e) {
+	$.addScreen.close();
+}
+
+function getMonthStr(i){
+	if(i>=0&&i<=11){
+		var monthStrArr = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+		return monthStrArr[i];
+	}else{
+		return '-';
+	}
+}
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,11 @@
-function doClick(e) {
-    alert($.label.text);
+function addNote(e) {
+	var addWindow = Alloy.createController('addScreen').getView();
+	addWindow.open();
+}
+
+function viewNote(e) {
+	var viewWindow = Alloy.createController('viewScreen').getView();
+	viewWindow.open();
 }
 
 $.index.open();
