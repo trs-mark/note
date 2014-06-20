52599c7deee2f5d56365a3e39fe9b1763cf2734b
trs-mark <trs.markt@gmail.com>
2014-06-20 15:50:18 +0800

    test archie

--- app/controllers/addScreen.js
+++ app/controllers/addScreen.js
@@ -2,8 +2,23 @@ function add(e){
 	var note = $.note.value;
 	if(note.trim().length > 0){
 		var newDate = new Date();
+		Alloy.Globals.notes.date= newDate.getDate();
+		Alloy.Globals.notes.time= newDate.getTime();
+		Alloy.Globals.notes.entry=note;
 		
 		alert('Note added.');
+		
+		.
+		
+		Alloy.Globals.notes.push({
+			'date':{month:'February',day:'14',year:'2014'},
+			'time':{hour:10,minute:0},
+			'entry':note
+		});
+		
+		
+		
+		
 	}else{
 		alert('Please type a note.');
 	}
